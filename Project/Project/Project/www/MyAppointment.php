4433b8e7ea12e32c7fc68ef087e84a9bf8e6559c
Ward Mousa <114559368+its-Ward@users.noreply.github.com>
2022-12-17 22:20:02 +0300

    WEB Project
    •	Select one of the following projects:
    •	Gym Class booking
    	Create database named: gym_db.
    	The database should include the following:
    •	classes
    o	classs_id
    o	trainer_name
    o	class_date.
    o	number_of_trainees
    o	max_num_of_trainees.
    •	bookings
    o	class_id.
    o	user_id.
    	Insert classes in the classes table.
    	Check list of available classes (only classes which did not exceed the max number of enrolled trainees is visible).
    	The user selects a class and confirm the booking.
    	The booking should be added to the database and the class number of enrolled trainees should be increased.
    •	  Course enrollment
    •	Create database named: course_enrollment_db.
    •	The database should include the following:
    •	Courses
    o	course_id
    o	course_code.
    o	course_name.
    o	number_enrolled_students.
    o	max_num_of_students.
    	enrollment
    o	course_id.
    o	user_id.
    	Basic functions
    o	Insert some data in the courses table.
    o	Check a list of courses (only courses which did not exceed the max number of enrolled trainees is visible).
    o	The user selects a class and confirm the booking.
    o	The booking should be added to the database and the class number of enrolled students is updated.
    •	Hospital appointment
    o	Create database named: hospital_db.
    o	The database should include the following:
    	appointments
    o	Appointment_id
    o	date.
    o	time.
    o	doctor_name.
    o	booking_status.
    	bookings
    o	Appointment_id.
    o	user_id.
    •	Basic functions
    o	Check a list of available dates and times (only free times is visible).
    o	Select a an appointment with a doctor.
    o	Confirm.
    o	The appointment booking should be added to the database.
    
    Requirements of the project
    •	Project Design Document (5 Marks)
    o	The document should include:
    	A basic list of function requirements.
    	An ER-diagram.
    	Page Hierarchy.
    •	Also include a description of each page, the description should include:
    o	Page title
    o	Content of each page.
    	UI Design.
    •	Page Layout
    •	Color Scheme.
    •	Font used.
    
    •	The Project (10 Marks)
    o	In the three system should allow the user to login and register.
    	The three system should have a user_account table.
    	username
    	email.
    	password.
    o	User proper html and css.
    •	Create a template that has the design of the website.
    •	Share the template on GitHub.
    •	All teams working on the project should use the template when working on their part of the website.
    •	Project folder organization:
    o	Create the following folders within Project folder:
    	Config
    •	db_login.php
    	src
    o	css
    o	for CSS files
    o	JS
    o	For Js files
    	PHP
    o	PHP pages that process information between the server an client. E.g: Login_action.php where login data is beging checked and sent to the server and the result sent back to the user.
    	www
    o	Files of your project…..
    Note: These pages are public and they are what the user access. E.g: Login.php / Login.html
    Note: the extension of the file depends on would use the php in the login page or just use html).

---
+++ Project/Project/Project/www/MyAppointment.php
@@ -0,0 +1,105 @@
+<?php session_start();
+
+if(!$_SESSION['id']){
+    Header("Location:login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+    <head>
+        <title>Home</title>
+        <link href="../src/css/style.css" rel="stylesheet" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300&display=swap" rel="stylesheet">
+    </head>
+<body>
+<nav>
+
+<ul>
+  <li><img src="../src/images/logo.png" width="45" height="45"></li>
+  <?php
+
+
+  if(isset($_SESSION['id'])){
+    ?>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="MyAppointment.php">My appointment</a></li>
+    <li style="float:right"><a class="active" href="logout.php">Logout</a></li>
+    <?php
+  }else{ ?>
+    <li><a href="index.php">Home</a></li>
+    <li style="float:right ; padding-left:2px"><a class="active" href="login.php">Login</a></li>
+    <li style="float:right"><a class="active" href="register.php">Register</a></li>
+
+    <?php
+  }
+  
+  ?>
+</ul>
+</nav>
+
+    <div class="main">
+        <div class="logo">
+            <img src="../src/images/logo.png">
+            <h2>Hospital appointment</h2>
+        </div>
+
+        <div class="book">
+        <span><?php echo (isset($_GET['message'])) ? $_GET['message'] : '' ?></span>
+        <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th style="width:35%%">Booking</th>
+                </tr>
+                    <?php
+                        include "../Config/db_login.php" ;
+                        foreach($conn->query("SELECT * FROM bookings b INNER JOIN appointments a on b.Appointment_id = a.Appointment_id WHERE b.user_id = ".$_SESSION['id']) as $appointment ){
+                            ?>
+                                <tr>
+                                    <td>Dr.<?php echo $appointment['doctor_name'] ?></td>
+                                    <td><?php echo $appointment['date'] ?></td>
+                                    <td><?php echo $appointment['time'] ?></td>
+                                <td>
+                                    <form action="" method="post">
+                                        <button style="background-color:#00d5dd;color:#454545" name = "Cancel" type="submit" value="<?php echo $appointment['Appointment_id'] ?>">Cancel</button>
+                                    </form>
+                                </td>
+                            <?php
+                        }
+                    ?>
+                </tr>
+            </table>
+
+
+
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <p>&copy; Copyright 2022</p>
+    </div>
+    </body>
+</html>
+
+
+<?php
+
+    if(isset($_POST['Cancel'])){
+        $Appointment_id = $_POST['Cancel'];
+        if($conn->query("DELETE FROM bookings WHERE `Appointment_id` ='$Appointment_id'")){
+            if($conn->query("UPDATE `appointments` SET `booking_status` = '0' WHERE `appointments`.`Appointment_id` =".$Appointment_id)){
+                Header("Location:MyAppointment.php?message=Successful delete");
+            }
+        }else{
+            Header("Location:MyAppointment.php?message=Error");
+        }
+    }
+
+?>
